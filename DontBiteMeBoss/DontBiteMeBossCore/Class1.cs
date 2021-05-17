a292aa9959743fd35db79fdb9621477521d2ee1d
PirkratekPL <pirkratek@gmail.com>
2021-05-17 14:05:04 +0200

    Created 3 new projects for Core, Server and Client

---
+++ DontBiteMeBoss/DontBiteMeBossCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DontBiteMeBossCore
+{
+    public class Class1
+    {
+    }
+}
---
+++ DontBiteMeBoss/DontBiteMeBossServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DontBiteMeBossServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
